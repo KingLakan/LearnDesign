00883487129a603df8e42a26d2b0dff0c897b91c
lakanlapp <lakanlapp@david.pb.samuelsson@gmail.com>
2018-12-22 21:55:43 +0100

    test med tillagd kommentar

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "SDL2/SDL.h"
 #include <stdio.h>
 #include "../include/WindowSingleton.h"
-
+//bajskorv
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
